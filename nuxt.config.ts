fb742cc903169f58450f500eff31e59cee0b0940
Daniel González García <daniel.gonzalez@libnamic.com>
2025-09-05 12:12:56 +0200

    supabase installation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.ts
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+
+  runtimeConfig: {
+    // Variables privadas (no accesibles desde el cliente)
+    supabaseServiceKey: process.env.SUPABASE_SERVICE_KEY,
+
+    public: {
+      // Variables públicas (accesibles desde el cliente)
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
+    }
+  }
 })

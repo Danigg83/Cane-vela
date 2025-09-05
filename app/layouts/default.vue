4078c71e1a4f03230a8533f15ccbe34767a0ec42
Daniel González García <daniel.gonzalez@libnamic.com>
2025-09-05 12:20:07 +0200

    Pasar header y footer a layouts

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,15 +1,33 @@
 <template>
-    <div class="min-h-screen bg-gray-50">
-      <!-- <Menu  v-if="$route.path !== '/gallery'" />
-        <Menu />-->
-      <main>
+    
+      <main  class="min-h-screen bg-gradient-to-br from-blue-100 via-blue-200 to-blue-300 flex flex-col">
+
+  <!-- Header -->
+  <header class="bg-white shadow-md py-6">
+        <div class="container mx-auto px-6 flex justify-between items-center">
+          <h1 class="text-3xl font-bold text-blue-700"><NuxtLink to="/">CANE - Vela</NuxtLink></h1>
+          <nav class="space-x-4">
+            <NuxtLink to=""></NuxtLink>
+            <a href="#" class="text-blue-600 hover:text-blue-800 font-medium">Inicio</a>
+            <a href="#" class="text-blue-600 hover:text-blue-800 font-medium">Acerca</a>
+            <a href="#" class="text-blue-600 hover:text-blue-800 font-medium">Contacto</a>
+          </nav>
+        </div>
+      </header>
+
         <NuxtPage />
+         <!-- Footer -->
+      <footer class="bg-white shadow-inner py-6 mt-auto">
+        <div class="container mx-auto text-center text-blue-600">
+          &copy; 2025 Mi Proyecto Nuxt. Todos los derechos reservados.
+        </div>
+      </footer>
+
       </main>
      
   
      
       
-    </div>
   </template>
   
   <script setup>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,18 +1,5 @@
 <template>
-    <main class="min-h-screen bg-gradient-to-br from-blue-100 via-blue-200 to-blue-300 flex flex-col">
       
-      <!-- Header -->
-      <header class="bg-white shadow-md py-6">
-        <div class="container mx-auto px-6 flex justify-between items-center">
-          <h1 class="text-3xl font-bold text-blue-700">Mi Proyecto Nuxt</h1>
-          <nav class="space-x-4">
-            <a href="#" class="text-blue-600 hover:text-blue-800 font-medium">Inicio</a>
-            <a href="#" class="text-blue-600 hover:text-blue-800 font-medium">Acerca</a>
-            <a href="#" class="text-blue-600 hover:text-blue-800 font-medium">Contacto</a>
-          </nav>
-        </div>
-      </header>
-  
       <!-- Hero Section -->
       <section class="flex-1 flex flex-col justify-center items-center text-center px-6">
         <h2 class="text-5xl font-extrabold text-blue-800 mb-4">¡Bienvenido a Nuxt 3 + Tailwind!</h2>
@@ -22,15 +9,7 @@
           <button class="bg-white text-blue-600 border border-blue-600 px-6 py-3 rounded-lg shadow hover:bg-blue-50 transition">Aprender más</button>
         </div>
       </section>
-  
-      <!-- Footer -->
-      <footer class="bg-white shadow-inner py-6 mt-auto">
-        <div class="container mx-auto text-center text-blue-600">
-          &copy; 2025 Mi Proyecto Nuxt. Todos los derechos reservados.
-        </div>
-      </footer>
-  
-    </main>
+     
   </template>
   
   <script setup>
